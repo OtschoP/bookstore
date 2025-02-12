93274341a9d4b7da71e9e4a70d7c0742b53a9e67
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-02-12 17:40:07 +0100

    added like Button

--- js/templates.js
+++ js/templates.js
@@ -4,12 +4,13 @@ function getBooksTemplate(book, index){
     <div class="book">
     <h2>${book.name}</h2>
     <p><b>Autor:</b> ${book.author}</p>
-    <p class="likes"><b>Likes: </b>${book.likes}</p>
+    <p class="likes"><b>Likes: </b>${book.likes}</p><button>like</button>
     <p><b>Preis: </b>${book.price} €</p>
     <p><b>Veröffentlicht: </b>${book.publishedYear}</p>
     <p><b>Genre: </b>${book.genre}</p>
     <h3>Kommentare</h3>
-    <input id="comment_input_${index}"  placeholder="Kommentar"></input><input id="commentName_input_${index}"  placeholder="Dein Name"></input><button onclick="addComment(${index})">kommentieren</button>
+    <input id="comment_input_${index}"  placeholder="Kommentar"></input><input id="commentName_input_${index}"  placeholder="Dein Name"></input>
+    <button onclick="addComment(${index})">kommentieren</button>
     <div id="comments_container_${index}">
     
     </div>
